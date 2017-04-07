e2889d68841b81efae535b2ecbffdcb118632164
Yovi Oktofianus <yovio@hotmail.com>
2017-04-07 15:24:09 +0800

    Add simple React
    Integrate with MS build pipeline

--- src/DockerBoard.Web/webpack.config.base.js
+++ src/DockerBoard.Web/webpack.config.base.js
@@ -3,11 +3,29 @@ global.webpack = require('webpack'); //to access webpack runtime
 
 module.exports = function () {
     return {
-        entry: ['./wwwroot/js/app.js'],
+        entry: ['./wwwroot/js/app.jsx'],
         output: {
             path: path.join(__dirname, '/wwwroot/assets'),
             filename: '[name].bundle.js',            
             sourceMapFilename: '[name].map'
-        }        
+        },
+        resolve: {
+            extensions: [".webpack.js", ".web.js", ".js", ".json", ".jsx"],
+            modules: ["./wwwroot/js", "node_modules"]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015', 'react']
+                        }
+                    }
+                }
+            ]
+        }
     };
 }
---
+++ src/DockerBoard.Web/wwwroot/js/app.jsx
@@ -0,0 +1,8 @@
+﻿import React from 'react';
+import { render } from 'react-dom';
+import HelloWorld from 'hello-world';
+
+render(
+    <HelloWorld />,
+    document.getElementById('rootDiv')
+);
