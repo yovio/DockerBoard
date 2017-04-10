19011be1feac4237741fcb04aed300f46cfc1c3b
Yovi Oktofianus <yovio@hotmail.com>
2017-04-10 16:51:21 +0800

    Replace with sample aspnetcore reactredux

--- src/DockerBoard.Web/webpack.config.js
+++ src/DockerBoard.Web/webpack.config.js
@@ -1,40 +1,77 @@
-﻿function isDevEnv(arg) {
-    if (arg == '-d' || arg.indexOf('webpack-dev-server') > -1) {
-        return true;
-    }
-    else
-        return false;
-}
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
+const merge = require('webpack-merge');
 
-function buildConfig(env) {
-    if (process.argv.find(isDevEnv))
-        return require('./webpack.config.dev.js')(env)
-    else
-        return require('./webpack.config.prod.js')(env)
-    
-}
+module.exports = (env) => {
+    const isDevBuild = !(env && env.prod);
 
-console.log(process.argv);
-module.exports = buildConfig;
+    // Configuration in common to both client-side and server-side bundles
+    const sharedConfig = () => ({
+        stats: { modules: false },
+        resolve: { extensions: [ '.js', '.jsx', '.ts', '.tsx' ] },
+        output: {
+            filename: '[name].js',
+            publicPath: '/dist/' // Webpack dev middleware, if enabled, handles requests for this URL prefix
+        },
+        module: {
+            rules: [
+                { test: /\.tsx?$/, include: /ClientApp/, use: 'babel-loader' },
+                { test: /\.tsx?$/, include: /ClientApp/, use: 'awesome-typescript-loader?silent=true' }
+            ]
+        },
+        plugins: [new CheckerPlugin()]
+    });
 
-//"use strict";
+    // Configuration for client-side bundle suitable for running in browsers
+    const clientBundleOutputDir = './wwwroot/dist';
+    const clientBundleConfig = merge(sharedConfig(), {
+        entry: { 'main-client': './ClientApp/boot-client.tsx' },
+        module: {
+            rules: [
+                { test: /\.css$/, use: ExtractTextPlugin.extract({ use: 'css-loader' }) },
+                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
+            ]
+        },
+        output: { path: path.join(__dirname, clientBundleOutputDir) },
+        plugins: [
+            new ExtractTextPlugin('site.css'),
+            new webpack.DllReferencePlugin({
+                context: __dirname,
+                manifest: require('./wwwroot/dist/vendor-manifest.json')
+            })
+        ].concat(isDevBuild ? [
+            // Plugins that apply in development builds only
+            new webpack.SourceMapDevToolPlugin({
+                filename: '[file].map', // Remove this line if you prefer inline source maps
+                moduleFilenameTemplate: path.relative(clientBundleOutputDir, '[resourcePath]') // Point sourcemap entries to the original file locations on disk
+            })
+        ] : [
+            // Plugins that apply in production builds only
+            new webpack.optimize.UglifyJsPlugin()
+        ])
+    });
 
-//module.exports = {
-//    entry: "./src/file.js",
-//    output: {
-//        filename: "./dist/bundle.js"
-//    },
-//    devServer: {
-//        contentBase: ".",
-//        host: "localhost",
-//        port: 9000
-//    },
-//    module: {
-//        loaders: [
-//            {
-//                test: /\.jsx?$/,
-//                loader: "babel-loader"
-//            }
-//        ]
-//    }
-//};
+    // Configuration for server-side (prerendering) bundle suitable for running in Node
+    const serverBundleConfig = merge(sharedConfig(), {
+        resolve: { mainFields: ['main'] },
+        entry: { 'main-server': './ClientApp/boot-server.tsx' },
+        plugins: [
+            new webpack.DllReferencePlugin({
+                context: __dirname,
+                manifest: require('./ClientApp/dist/vendor-manifest.json'),
+                sourceType: 'commonjs2',
+                name: './vendor'
+            })
+        ],
+        output: {
+            libraryTarget: 'commonjs',
+            path: path.join(__dirname, './ClientApp/dist')
+        },
+        target: 'node',
+        devtool: 'inline-source-map'
+    });
+
+    return [clientBundleConfig, serverBundleConfig];
+};
--- src/DockerBoard.Web/wwwroot/js/app.jsx
+++
@@ -1,8 +0,0 @@
-﻿import React from 'react';
-import { render } from 'react-dom';
-import HelloWorld from 'hello-world';
-
-render(
-    <HelloWorld />,
-    document.getElementById('rootDiv')
-);
